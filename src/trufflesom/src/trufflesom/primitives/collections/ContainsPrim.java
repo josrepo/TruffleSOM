3428e05088e152b46366f6f49d8e6a0fc45cedc4
josrepo <18370334+josrepo@users.noreply.github.com>
2024-01-25 17:30:22 +0000

    Finish contains and indexof

--- src/trufflesom/src/trufflesom/primitives/collections/ContainsPrim.java
+++ src/trufflesom/src/trufflesom/primitives/collections/ContainsPrim.java
@@ -2,8 +2,14 @@ package trufflesom.primitives.collections;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
 import trufflesom.bdt.primitives.Primitive;
 import trufflesom.interpreter.nodes.nary.BinaryMsgExprNode;
+import trufflesom.primitives.basics.BlockPrimsFactory;
+import trufflesom.primitives.basics.EqualsPrim;
+import trufflesom.primitives.basics.EqualsPrimFactory;
+import trufflesom.primitives.basics.LengthPrimFactory;
 import trufflesom.vm.SymbolTable;
 import trufflesom.vmobjects.SSymbol;
 import trufflesom.vmobjects.SVector;
@@ -12,19 +18,26 @@ import trufflesom.vmobjects.SVector;
 @Primitive(className = "Vector", primitive = "contains:", selector = "contains:", receiverType = SVector.class, inParser = false)
 public abstract class ContainsPrim extends BinaryMsgExprNode {
 
+  @Child private EqualsPrim equals;
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T extends Node> T initialize(final long coord) {
+    super.initialize(coord);
+    equals = EqualsPrimFactory.create(null, null);
+    return (T) this;
+  }
+
   @Specialization(guards = "receiver.isObjectType()")
-  public static final boolean doObjectSVector(final SVector receiver, final Object value) {
-    if (false /* need to handle strings differently */) {
-      // string stuff
-      return false;
-    } else {
+  public final boolean doObjectSVector(final VirtualFrame frame, final SVector receiver, final Object value) {
       final Object[] storage = receiver.getObjectStorage();
-      for (int i = receiver.getFirstIndex() - 1; i < receiver.getLastIndex(); i++) {
-        if (storage[i] == value) {
+      int last = receiver.getLastIndex();
+
+      for (int i = receiver.getFirstIndex() - 1; i < last; i++) {
+        if (this.equals.makeGenericSend(frame, storage[i], value).equals(true)) {
           return true;
         }
       }
-    }
 
     return false;
   }
--- src/trufflesom/src/trufflesom/primitives/collections/IndexOfPrim.java
+++ src/trufflesom/src/trufflesom/primitives/collections/IndexOfPrim.java
@@ -2,8 +2,12 @@ package trufflesom.primitives.collections;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
 import trufflesom.bdt.primitives.Primitive;
 import trufflesom.interpreter.nodes.nary.BinaryMsgExprNode;
+import trufflesom.primitives.basics.EqualsPrim;
+import trufflesom.primitives.basics.EqualsPrimFactory;
 import trufflesom.vm.SymbolTable;
 import trufflesom.vmobjects.SSymbol;
 import trufflesom.vmobjects.SVector;
@@ -12,23 +16,27 @@ import trufflesom.vmobjects.SVector;
 @Primitive(className = "Vector", primitive = "indexOf:", selector = "indexOf:", receiverType = SVector.class, inParser = false)
 public abstract class IndexOfPrim extends BinaryMsgExprNode {
 
-  @Specialization(guards = "receiver.isObjectType()")
-  public static final long doObjectSVector(final SVector receiver, final Object value) {
-    if (false /* need to handle strings differently??? */) {
-      // need to test how som handles index of with string. test with vector:
-      // [1, "two", "three", "two"]
-      // assume it does string equality so indexOf("two") would return the first one
+  @Child private EqualsPrim equals;
 
-      // string stuff
-      return -1;
-    } else {
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T extends Node> T initialize(final long coord) {
+    super.initialize(coord);
+    equals = EqualsPrimFactory.create(null, null);
+    return (T) this;
+  }
+
+  @Specialization(guards = "receiver.isObjectType()")
+  public final long doObjectSVector(final VirtualFrame frame, final SVector receiver, final Object value) {
       final Object[] storage = receiver.getObjectStorage();
-      for (int i = receiver.getFirstIndex() - 1; i < receiver.getLastIndex(); i++) {
-        if (storage[i] == value) {
-          return i - receiver.getFirstIndex() + 2;
+      int first = receiver.getFirstIndex();
+      int last = receiver.getLastIndex();
+
+      for (int i = first - 1; i < last; i++) {
+        if (this.equals.makeGenericSend(frame, storage[i], value).equals(true)) {
+          return i - first + 2;
         }
       }
-    }
 
     return -1;
   }
