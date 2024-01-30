5e76a9b27d4f088e34f478b09499a6d7803c988f
josrepo <18370334+josrepo@users.noreply.github.com>
2024-01-30 22:00:52 +0000

    Add strategies to remove last prim

--- src/trufflesom/src/trufflesom/primitives/vectors/RemoveLastPrim.java
+++ src/trufflesom/src/trufflesom/primitives/vectors/RemoveLastPrim.java
@@ -13,6 +13,17 @@ import trufflesom.vmobjects.SVector;
 @Primitive(className = "Vector", primitive = "remove", selector = "remove", receiverType = SVector.class, inParser = false)
 public abstract class RemoveLastPrim extends UnaryExpressionNode {
 
+  @Specialization(guards = "receiver.isEmptyType()")
+  public final Object doEmptySVector(final VirtualFrame frame, final SVector receiver) {
+    if (receiver.getSize() > 0) {
+      receiver.decrementLastIndex();
+      return Nil.nilObject;
+    } else {
+      return makeGenericSend(SymbolTable.symbolFor("error:"))
+          .doPreEvaluated(frame, new Object[] {receiver, "Vector: Attempting to remove the first element from an empty Vector"});
+    }
+  }
+
   @Specialization(guards = "receiver.isObjectType()")
   public final Object doObjectSVector(final VirtualFrame frame, final SVector receiver) {
     if (receiver.getSize() > 0) {
@@ -22,6 +33,36 @@ public abstract class RemoveLastPrim extends UnaryExpressionNode {
       final Object value = storage[last];
       storage[last] = Nil.nilObject;
       return value == null ? Nil.nilObject : value;
+    } else {
+      return makeGenericSend(SymbolTable.symbolFor("error:"))
+          .doPreEvaluated(frame, new Object[] {receiver, "Vector: Attempting to remove the last element from an empty Vector"});
+    }
+  }
+
+  @Specialization(guards = "receiver.isLongType()")
+  public final Object doLongSVector(final VirtualFrame frame, final SVector receiver) {
+    if (receiver.getSize() > 0) {
+      receiver.decrementLastIndex();
+      final long[] storage = receiver.getLongStorage();
+      int last = receiver.getLastIndex() - 1;
+      final long value = storage[last];
+      storage[last] = SVector.EMPTY_LONG_SLOT;
+      return value == SVector.EMPTY_LONG_SLOT ? Nil.nilObject : value;
+    } else {
+      return makeGenericSend(SymbolTable.symbolFor("error:"))
+          .doPreEvaluated(frame, new Object[] {receiver, "Vector: Attempting to remove the last element from an empty Vector"});
+    }
+  }
+
+  @Specialization(guards = "receiver.isDoubleType()")
+  public final Object doDoubleSVector(final VirtualFrame frame, final SVector receiver) {
+    if (receiver.getSize() > 0) {
+      receiver.decrementLastIndex();
+      final double[] storage = receiver.getDoubleStorage();
+      int last = receiver.getLastIndex() - 1;
+      final double value = storage[last];
+      storage[last] = SVector.EMPTY_DOUBLE_SLOT;
+      return value == SVector.EMPTY_DOUBLE_SLOT ? Nil.nilObject : value;
     } else {
       return makeGenericSend(SymbolTable.symbolFor("error:"))
           .doPreEvaluated(frame, new Object[] {receiver, "Vector: Attempting to remove the last element from an empty Vector"});
