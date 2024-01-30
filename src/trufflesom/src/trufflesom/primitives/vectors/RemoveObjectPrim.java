3141e5b818cecc596b93b915de437c88f2be9624
josrepo <18370334+josrepo@users.noreply.github.com>
2024-01-30 22:21:45 +0000

    Add strategies to remove prim

--- src/trufflesom/src/trufflesom/primitives/vectors/RemoveObjectPrim.java
+++ src/trufflesom/src/trufflesom/primitives/vectors/RemoveObjectPrim.java
@@ -9,6 +9,7 @@ import trufflesom.interpreter.nodes.nary.BinaryMsgExprNode;
 import trufflesom.primitives.basics.EqualsEqualsPrim;
 import trufflesom.primitives.basics.EqualsEqualsPrimFactory;
 import trufflesom.vm.SymbolTable;
+import trufflesom.vm.constants.Nil;
 import trufflesom.vmobjects.SSymbol;
 import trufflesom.vmobjects.SVector;
 
@@ -18,12 +19,35 @@ public abstract class RemoveObjectPrim extends BinaryMsgExprNode {
 
   @Child private EqualsEqualsPrim equalsEquals;
 
+  protected static final boolean valueIsNil(final Object value) {
+    return value == Nil.nilObject;
+  }
+
+  protected static final boolean valueIsNotNil(final Object value) {
+    return value != Nil.nilObject;
+  }
+
   @Override
   @SuppressWarnings("unchecked")
   public <T extends Node> T initialize(final long coord) {
     super.initialize(coord);
     equalsEquals = EqualsEqualsPrimFactory.create(null, null);
     return (T) this;
+  }
+
+  @Specialization(guards = {"receiver.isEmptyType()", "valueIsNotNil(value)"})
+  @SuppressWarnings("unused")
+  public final boolean doEmptySVector(final VirtualFrame frame, final SVector receiver, final Object value) {
+    return false;
+  }
+
+  @Specialization(guards = {"receiver.isEmptyType()", "valueIsNil(value)"})
+  @SuppressWarnings("unused")
+  public final boolean doEmptySVectorWithNil(final VirtualFrame frame, final SVector receiver, final Object value) {
+    receiver.setLastIndex(1);
+    receiver.resetFirstIndex();
+
+    return true;
   }
 
   @Specialization(guards = "receiver.isObjectType()")
@@ -36,6 +60,56 @@ public abstract class RemoveObjectPrim extends BinaryMsgExprNode {
 
     for (int i = receiver.getFirstIndex() - 1; i < last; i++) {
       if (this.equalsEquals.executeEvaluated(frame, storage[i], value).equals(true)) {
+        found = true;
+      } else {
+        newStorage[i] = storage[i];
+        newLast++;
+      }
+    }
+
+    receiver.setStorage(newStorage);
+    receiver.setLastIndex(newLast);
+    receiver.resetFirstIndex();
+
+    return found;
+  }
+
+  @Specialization(guards = "receiver.isLongType()")
+  @SuppressWarnings("unused")
+  public final boolean doLongSVector(final VirtualFrame frame, final SVector receiver, final long value) {
+    final long[] storage = receiver.getLongStorage();
+    final long[] newStorage = new long[storage.length];
+    int last = receiver.getLastIndex() - 1;
+    int newLast = 1;
+    boolean found = false;
+
+    for (int i = receiver.getFirstIndex() - 1; i < last; i++) {
+      if (storage[i] == value) {
+        found = true;
+      } else {
+        newStorage[i] = storage[i];
+        newLast++;
+      }
+    }
+
+    receiver.setStorage(newStorage);
+    receiver.setLastIndex(newLast);
+    receiver.resetFirstIndex();
+
+    return found;
+  }
+
+  @Specialization(guards = "receiver.isDoubleType()")
+  @SuppressWarnings("unused")
+  public final boolean doDoubleSVector(final VirtualFrame frame, final SVector receiver, final double value) {
+    final double[] storage = receiver.getDoubleStorage();
+    final double[] newStorage = new double[storage.length];
+    int last = receiver.getLastIndex() - 1;
+    int newLast = 1;
+    boolean found = false;
+
+    for (int i = receiver.getFirstIndex() - 1; i < last; i++) {
+      if (storage[i] == value) {
         found = true;
       } else {
         newStorage[i] = storage[i];
