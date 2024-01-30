9e428db52a19ea13415b582f606f1eb60b342426
josrepo <18370334+josrepo@users.noreply.github.com>
2024-01-30 23:16:27 +0000

    Fix capacity prim

--- src/trufflesom/src/trufflesom/primitives/vectors/CapacityPrim.java
+++ src/trufflesom/src/trufflesom/primitives/vectors/CapacityPrim.java
@@ -10,14 +10,24 @@ import trufflesom.vmobjects.SVector;
 @Primitive(className = "Vector", primitive = "capacity", selector = "capacity", receiverType = SVector.class, inParser = false)
 public abstract class CapacityPrim extends UnaryExpressionNode {
 
-  @Specialization(guards = "!receiver.isEmptyType()")
-  public static final long doNonEmptySVector(final SVector receiver) {
-    return receiver.getObjectStorage().length;
-  }
-
   @Specialization(guards = "receiver.isEmptyType()")
   public static final long doEmptySVector(final SVector receiver) {
     return receiver.getEmptyStorage();
+  }
+
+  @Specialization(guards = "receiver.isObjectType()")
+  public static final long doObjectSVector(final SVector receiver) {
+    return receiver.getObjectStorage().length;
+  }
+
+  @Specialization(guards = "receiver.isLongType()")
+  public static final long doLongSVector(final SVector receiver) {
+    return receiver.getLongStorage().length;
+  }
+
+  @Specialization(guards = "receiver.isDoubleType()")
+  public static final long doDoubleSVector(final SVector receiver) {
+    return receiver.getDoubleStorage().length;
   }
 
 }
