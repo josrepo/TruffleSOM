dcdbfb73a06a0c16d96e6b70c89926adbc5ecfbe
josrepo <18370334+josrepo@users.noreply.github.com>
2024-02-01 18:43:53 +0000

    Add strategies to as array prim

--- src/trufflesom/src/trufflesom/primitives/vectors/AsArrayPrim.java
+++ src/trufflesom/src/trufflesom/primitives/vectors/AsArrayPrim.java
@@ -13,11 +13,31 @@ import java.util.Arrays;
 @Primitive(className = "Vector", primitive = "asArray", selector = "asArray", receiverType = SVector.class, inParser = false)
 public abstract class AsArrayPrim extends UnaryExpressionNode {
 
+  @Specialization(guards = "receiver.isEmptyType()")
+  @SuppressWarnings("unused")
+  public static final SArray doEmptySVector(final SVector receiver) {
+    return SArray.create(0);
+  }
+
   @Specialization(guards = "receiver.isObjectType()")
-  public static final SArray doSVector(final SVector receiver) {
+  public static final SArray doObjectSVector(final SVector receiver) {
     return SArray.create(Arrays.copyOfRange(receiver.getObjectStorage(),
         receiver.getFirstIndex() - 1,
         receiver.getLastIndex() - 1));
   }
-  
+
+  @Specialization(guards = "receiver.isLongType()")
+  public static final SArray doLongSVector(final SVector receiver) {
+    return SArray.create(Arrays.copyOfRange(receiver.getLongStorage(),
+        receiver.getFirstIndex() - 1,
+        receiver.getLastIndex() - 1));
+  }
+
+  @Specialization(guards = "receiver.isDoubleType()")
+  public static final SArray doDoubleSVector(final SVector receiver) {
+    return SArray.create(Arrays.copyOfRange(receiver.getDoubleStorage(),
+        receiver.getFirstIndex() - 1,
+        receiver.getLastIndex() - 1));
+  }
+
 }
